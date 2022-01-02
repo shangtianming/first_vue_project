9b65b104f2c61f27b375e9abf20eda11b4484ff8
yinwenbin <yin921125@163.com>
2022-01-02 20:02:52 +0800

    '测试pipline'

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -1,14 +1,16 @@
+// Declarative类型 
 pipeline {
     agent any 
     stages {
         stage('Build') { 
             steps {
+                // 这里是Groovy语法，写了一个打印语句
                 println "Build" 
             }
         }
         stage('Test') { 
             steps {
-                println "Test" 
+                echo $JAVA_HOME 
             }
         }
         stage('Deploy') { 
@@ -18,3 +20,16 @@ pipeline {
         }
     }
 }
+
+// Scripted模式
+// node {  
+//     stage('Build') { 
+//         // 
+//     }
+//     stage('Test') { 
+//         // 
+//     }
+//     stage('Deploy') { 
+//         // 
+//     }
+// }
