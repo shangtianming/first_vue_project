85a418cc94417abd564fcbfed5a58ab083feed9f
yinwenbin <yin921125@163.com>
2022-01-02 20:08:03 +0800

    '添加jenkinsfile'

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -10,7 +10,7 @@ pipeline {
         }
         stage('Test') { 
             steps {
-                echo $JAVA_HOME 
+                echo `pwd`
             }
         }
         stage('Deploy') { 
