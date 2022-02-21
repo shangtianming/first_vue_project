e068e8edbc197353eeaeaee77e723f4e85be8e0d
wenbin.yin <wenbin.yin@oriente.com>
2022-02-21 17:17:22 +0800

    test_jenkins

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -12,23 +12,5 @@ pipeline {
                 }
             }
         }
-		stage('Utility Steps method---readProperties') {
-		    steps {
-		        script {
-					properties_file = env.WORKSPACE + "/test_jenkins.properties"
-		            props = readProperties interpolate: true, file: properties_file
-		            println props
-		        }
-		    }
-		}
-		stage('Utility Steps method---readYaml') {
-		    steps {
-		        script {
-					yaml_file = env.WORKSPACE + "/test_jenkins.yaml"
-		            y = readYaml file : yaml_file
-		            println y
-		        }
-		    }
-		}
     }
 }
