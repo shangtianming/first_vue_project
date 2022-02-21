3b1f91d777ce497647b7adf886d627664d34b134
wenbin.yin <wenbin.yin@oriente.com>
2022-02-21 18:37:46 +0800

    test_jenkins

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -4,9 +4,15 @@ pipeline{
 		stage("dir") {
 			steps{
 			    println env.WORKSPACE
+				// 如果没有这个目录，会自动创建这个目录
 			    dir("${env.WORKSPACE}/testdata"){
 				    sh "pwd"
 			    }
+				
+				echo ("list all files under current workd directory")
+				sh("ls -al ${env.WORKSPACE}")
+				echo("some info logs output")
+				error("some error method output")
 			}
 		}
 	}
