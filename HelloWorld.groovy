d254d4f513107071ade70117a326fa757b44653e
wenbin.yin <wenbin.yin@oriente.com>
2022-02-22 18:32:55 +0800

    test_jenkins

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -5,12 +5,11 @@ pipeline{
 		stage("fileExists") {
 			steps{
 				script {
-					json_file = "${env.WORKSPACE}/testdata/test_json.json"
-					if(fileExists(json_file) == true) {
-						echo("json file is exists")
-					}else {
-						error("here haven't find json file")
-					}
+					if(isUnix() == true) {
+											echo("this jenkins job running on a linux-like system")
+										}else {
+											error("the jenkins job running on a windows system")
+										}
 				}
 			}
 		}
