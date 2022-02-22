4a1288b2a2dec7e21b5fcd84834493731973e6c4
wenbin.yin <wenbin.yin@oriente.com>
2022-02-22 18:53:00 +0800

    test_jenkins

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -1,25 +1,54 @@
 pipeline{
-	
-	agent any
-	stages{
+    agent any
+    stages{
+        stage("dir") {
+            steps{
+                // 如果没有这个目录，会自动创建这个目录
+                dir("${env.WORKSPACE}/testdata"){
+                    sh "pwd"
+                }
+            }
+        }
 		stage("fileExists") {
 			steps{
 				script {
-					if(isUnix() == true) {
-											echo("this jenkins job running on a linux-like system")
-										}else {
-											error("the jenkins job running on a windows system")
-										}
+					json_file = "${env.WORKSPACE}/test_jenkins.json"
+					if(fileExists(json_file) == true) {
+						echo("json file is exists")
+					}else {
+						error("here haven't find json file")
+					}
 				}
 			}
 		}
-	}
-	post {
-		always {
-			script {
-				// deleteDir()
-				println "结束"
+		stage("isUnix") {
+			steps{
+				script {
+					if(isUnix() == true) {
+						echo("this jenkins job running on a linux-like system")
+					}else {
+						error("the jenkins job running on a windows system")
+					}
+				}
 			}
 		}
-	}
+		stage("fileExists") {
+			steps{
+				script {
+					sh("pwd")
+					println "==========="
+					println pwd()
+				}
+			}
+		}
+    }
+    post {
+        always {
+            script {
+                //写相关清除/恢复环境等操作代码
+                //deleteDir()
+                println "结束"
+            }
+        }
+    }
 }
