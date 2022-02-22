9bd8fb756e27590f71c542a508cf9bf64395ad30
wenbin.yin <wenbin.yin@oriente.com>
2022-02-22 18:21:37 +0800

    test_jenkins

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -1,32 +1,17 @@
 pipeline{
+	
 	agent any
 	stages{
-		stage("dir") {
+		stage("fileExists") {
 			steps{
-			    println env.WORKSPACE
-				// 如果没有这个目录，会自动创建这个目录
-			    dir("${env.WORKSPACE}/testdata"){
-				    sh "pwd"
-			    }
-				
-				echo ("list all files under current workd directory")
-				sh("ls -al ${env.WORKSPACE}")
-				echo("some info logs output")
-				error("some error method output")
-			}
-		}
-		stage("test") {
-			steps{
-				echo("some info logs output=======test")
-			}
-		}
-	}
-	post {
-		always {
-			script {
-				//写相关清除/恢复环境等操作代码
-				//deleteDir()
-				println "结束"
+				script {
+					json_file = "${env.WORKSPACE}/testdata/test_json.json"
+					if(fileExists(json_file) == true) {
+						echo("json file is exists")
+					}else {
+						error("here haven't find json file")
+					}
+				}
 			}
 		}
 	}
