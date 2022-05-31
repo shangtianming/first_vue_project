7f546af84f20ea72b37f63696b2d0749a538ff9f
yinwenbin <yin921125@163.com>
2022-05-31 21:46:12 +0800

    五月最后一天2

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -1,82 +1,28 @@
 pipeline{
 	agent any
-	environment {
-		to_email = "yin921125@qq.com"
-		to_email_address_list = "yin921125@qq.com,yin921125@163.com"
-	}
 	stages{
-		stage("发邮件demo 1") {
+		stage("init") {
 			steps{
 				script {
-					mail to: to_email,
-					subject: "Running Pipeline: ${currentBuild.fullDisplayName}",
-					body: "Something is wrong with ${env.BUILD_URL}"
+					json_file = "${env.WORKSPACE}/package.json"
+					file_contents = readFile json_file
+					println file_contents
 				}
 			}
 		}
-		stage("发邮件demo 2") {
+		stage("retry and sleep") {
 			steps{
-				script {
-					mail to: to_email,
-					cc: 'xxxxx@qq.com',			// 抄送
-					charset:'UTF-8',			// or GBK/GB18030
-					mimeType:'text/plain',		// or text/html
-					subject: "Running Pipeline: ${currentBuild.fullDisplayName}",
-					body: "Something is wrong with ${env.BUILD_URL}, just for test send mail via pipeline code"
+				script{
+					try {
+						retry(3) {
+							println "here we are test retry fuction"
+							sleep 5
+							println 10/0
+						}
+					}catch (Exception e) {
+						println e
+					}
 				}
-			}
-		}
-		stage("发邮件demo 3") {
-			steps{
-				script {
-					subject = "Jenkins Job : " + env.JOB_NAME + "/" + env.BUILD_ID
-					result_url = env.BUILD_URL + "console"
-					text = """
-					<!DOCTYPE html>
-					<html>
-						<head>
-							<meta charset="utf-8">
-							<title></title>
-						</head>
-						<body>
-							<div>
-								<h1>Summary</h1>
-								<div>
-									<h2>Jenkins Build</h2>
-									<ul>
-										<li>Job URL : <a href='${env.BUILD_URL}'>${env.BUILD_URL}</a></li>
-										<li>Build Result URL : <a href='${result_url}'>${result_url}</a></li>
-									</ul>
-								</div>
-								<div>
-									<h2>GIT Branch</h2>
-									<ul>
-										<li>这是一个测试分支</li>
-									</ul>
-								</div>
-							</div>
-						</body>
-					</html>
-					"""
-					mail body: text, subject: subject,  mimeType: 'text/html', to: to_email_address_list
-				}
-			}
-		}
-	}
-	post{
-		always{
-			script {
-				module_test = load env.WORKSPACE + "/test_groovy.groovy"
-			}
-		}
-		failure {
-			script {
-				module_test.send_email_results("Failed","Master",to_email_address_list)
-			}
-		}
-		success {
-			script {
-				module_test.send_email_results("Success","Master",to_email_address_list)
 			}
 		}
 	}
