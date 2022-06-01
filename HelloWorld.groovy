4bc07c8f1747d7256f36b98bf514835eb4ebf892
yinwenbin <yin921125@163.com>
2022-06-01 13:08:53 +0800

    06-01-01

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -4,23 +4,26 @@ pipeline{
 		stage("init") {
 			steps{
 				script {
-					json_file = "${env.WORKSPACE}/package.json"
-					file_contents = readFile json_file
-					println file_contents
+					// 设置超时5s，默认单位是分钟
+					timeout(time: 5, unit: 'SECONDS') {
+						//initialRecurrencePeriod ：设置重试之间的初始等待时间（以毫秒为单位）。
+						// 默认为 250 毫秒。每次失败都会将尝试之间的延迟减慢至最多 15 秒
+						//quiet ：如果为真，则每次检查条件时该步骤都不会记录消息。默认为假。类型： boolean
+						waitUntil(initialRecurrencePeriod: 1000,quiet: true){
+							script {
+								isUnix()
+							}
+						}
+					}
 				}
 			}
 		}
-		stage("retry and sleep") {
+		stage("env") {
 			steps{
-				script{
-					try {
-						retry(3) {
-							println "here we are test retry fuction"
-							sleep 5
-							println 10/0
-						}
-					}catch (Exception e) {
-						println e
+				script {
+					withEnv(['java_home=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-3.b13.el6_10.i386/jre']) {
+						sh("$java_home/bin/java -version")
+						println "test with withEnv feature"
 					}
 				}
 			}
