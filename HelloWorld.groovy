9844ae638ba4c33abfeb169ff59a2a2356d5a68d
yinwenbin <yin921125@163.com>
2022-06-01 13:23:42 +0800

    06-01-01

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -9,7 +9,7 @@ pipeline{
 						//initialRecurrencePeriod ：设置重试之间的初始等待时间（以毫秒为单位）。
 						// 默认为 250 毫秒。每次失败都会将尝试之间的延迟减慢至最多 15 秒
 						//quiet ：如果为真，则每次检查条件时该步骤都不会记录消息。默认为假。类型： boolean
-						println isUnix()
+						println "是unix系统则返回True" + isUnix()
 						waitUntil(initialRecurrencePeriod: 1000,quiet: true){
 							script {
 								isUnix()
