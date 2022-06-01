f7bfcbd655af15f9bb83861573078ae28a24b0ca
yinwenbin <yin921125@163.com>
2022-06-01 13:19:47 +0800

    06-01-01

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -5,7 +5,7 @@ pipeline{
 			steps{
 				script {
 					// 设置超时5s，默认单位是分钟
-					timeout(time: 5, unit: 'SECONDS') {
+					timeout(time: 50, unit: 'SECONDS') {
 						//initialRecurrencePeriod ：设置重试之间的初始等待时间（以毫秒为单位）。
 						// 默认为 250 毫秒。每次失败都会将尝试之间的延迟减慢至最多 15 秒
 						//quiet ：如果为真，则每次检查条件时该步骤都不会记录消息。默认为假。类型： boolean
