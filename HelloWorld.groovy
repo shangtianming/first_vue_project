27407bd04fe9a98c01372604692df03c39f9fdbb
yinwenbin <yin921125@163.com>
2022-06-01 13:50:38 +0800

    06-01-01

--- HelloWorld.groovy
+++ HelloWorld.groovy
@@ -1,31 +1,20 @@
+println env.JOB_NAME
+println env.BUILD_NUMBER
+
 pipeline{
+
 	agent any
 	stages{
-		stage("init") {
+		stage("writeFile demo") {
 			steps{
 				script {
-					// 设置超时5s，默认单位是分钟
-					timeout(time: 50, unit: 'SECONDS') {
-						//initialRecurrencePeriod ：设置重试之间的初始等待时间（以毫秒为单位）。
-						// 默认为 250 毫秒。每次失败都会将尝试之间的延迟减慢至最多 15 秒
-						//quiet ：如果为真，则每次检查条件时该步骤都不会记录消息。默认为假。类型： boolean
-						println "是unix系统则返回True" + isUnix()
-						waitUntil(initialRecurrencePeriod: 1000,quiet: true){
-							script {
-								isUnix()
-							}
-						}
-					}
-				}
-			}
-		}
-		stage("env") {
-			steps{
-				script {
-					withEnv(['java_home=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-3.b13.el6_10.i386/jre']) {
-						sh("$java_home/bin/java -version")
-						println "test with withEnv feature"
-					}
+					write_file_path = "${env.WORKSPACE}/test_jenkins.txt"
+					file_contents = "Hello Anthony!! 这是一个测试例子"
+					// 写文件
+					writeFile file: write_file_path, text: file_contents, encoding: "UTF-8"
+					// 读文件
+					fileContents = readFile file: write_file_path, encoding: "UTF-8"
+					println fileContents
 				}
 			}
 		}
